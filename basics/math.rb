a8b9538f41885ebb2e5f607fe0203949c23f45c7
rmarkus231 <75732726+rmarkus231@users.noreply.github.com>
2022-04-05 17:27:09 +0300

    sorted

---
+++ basics/math.rb
@@ -0,0 +1,15 @@
+n1 = 5
+n2 = 2
+
+#liitmine
+puts num1 + num2
+#lahutamine
+putsa num1 - num2
+#korrutamine
+puts num1 * num2
+#jagamine
+puts num1 / num2
+#ruutjuur
+puts Math.sqrt(num1)
+#astmed
+puts num1 ** num2
